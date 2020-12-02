84d9ff959439ce387cfb003c76295fe3f03e21c6
changdingfang <245292011@qq.com>
2020-12-02 08:49:14 +0800

    googletest

--- googletest/googleTest.cpp
+++ googletest/googleTest.cpp
@@ -3,11 +3,15 @@
 // Author:       dingfang
 // CreateDate:   2020-12-01 21:14:23
 // ModifyAuthor: dingfang
-// ModifyDate:   2020-12-02 08:17:00
+// ModifyDate:   2020-12-02 08:48:29
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-#include <gtest/gtest.h>
+/*
+ * EXPECT_* 版本的断言失败时产生非致命，不会结束当前函数
+ * ASSERT_* 版本断言失败时会产生致命失败，并结束当前函数
+ * */
 
+#include <gtest/gtest.h>
 
 int add(int a, int b)
 {
@@ -26,12 +30,18 @@ TEST(test, add1)
 
 TEST(test, t2)
 {
-    EXPECT_STREQ("hello world", "hello world");       /* == */
-    EXPECT_STRNE("hello world", "world");       /* != */
-    EXPECT_STRCASEEQ("hello world", "HELLO WORLD");   /* == 忽略大小写 */
-    EXPECT_STRCASENE("hello world", "world");   /* != 忽略大小写 */
+    EXPECT_STREQ("hello world", "hello world");         /* == */
+    EXPECT_STRNE("hello world", "world");               /* != */
+    EXPECT_STRCASEEQ("hello world", "HELLO WORLD");     /* == 忽略大小写 */
+    EXPECT_STRCASENE("hello world", "world");           /* != 忽略大小写 */
 
-    EXPECT_STRCASENE("hello world", "HELLO WORLD");   /* error  != 忽略大小写 */
+    // EXPECT_STRCASENE("hello world", "HELLO WORLD");   /* error  != 忽略大小写 */
+}
+
+TEST(test, t3)
+{
+    ASSERT_EQ(add(3, 5), 9);
+    ASSERT_EQ(add(3, 5), 8);
 }
 
 int main(void)
